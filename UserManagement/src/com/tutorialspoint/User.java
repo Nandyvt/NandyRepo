78124360bfc6a728db6a58b33a74fac51ab828df
Nandeesh <Nandy@vtnandeesh>
2016-08-27 20:44:01 +0530

    sdwdw

--- UserManagement/src/com/tutorialspoint/User.java
+++ UserManagement/src/com/tutorialspoint/User.java
@@ -22,7 +22,7 @@ public class User implements Serializable {
       this.id = id;
       this.name = name;
       this.profession = profession;
-      System.out.println("");
+      System.out.println("Nandy");
    }
 
    public int getId() {
